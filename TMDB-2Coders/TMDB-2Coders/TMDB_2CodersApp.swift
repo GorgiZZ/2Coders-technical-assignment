1e7aaae34628275dc3d8fb4d5815184bf8b6e35c
GorgiZZ <gorgiz.97z@gmail.com>
2025-05-09 22:51:11 +0200

    Added project to repo

---
+++ TMDB-2Coders/TMDB-2Coders/TMDB_2CodersApp.swift
@@ -0,0 +1,32 @@
+//
+//  TMDB_2CodersApp.swift
+//  TMDB-2Coders
+//
+//  Created by Gjorgji Zhupan on 9.5.25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TMDB_2CodersApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ TMDB-2Coders/TMDB-2CodersUITests/TMDB_2CodersUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  TMDB_2CodersUITestsLaunchTests.swift
+//  TMDB-2CodersUITests
+//
+//  Created by Gjorgji Zhupan on 9.5.25.
+//
+
+import XCTest
+
+final class TMDB_2CodersUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
